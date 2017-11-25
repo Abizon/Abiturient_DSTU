3cee41256ac18a2e4c8b8fefe542470b1d4b699e
dieselcoding <dieselcoding@gmail.com>
2017-11-25 17:27:29 +0300

    Diesel Commit

--- app/src/main/java/com/dstu/myapplication/dstu/Requests.java
+++ app/src/main/java/com/dstu/myapplication/dstu/Requests.java
@@ -1,6 +1,7 @@
 package com.dstu.myapplication.dstu;
 
 import com.dstu.myapplication.models.Event;
+import com.dstu.myapplication.models.News;
 import com.dstu.myapplication.models.Specialty;
 import com.dstu.myapplication.models.Facultie;
 
@@ -14,4 +15,6 @@ public interface Requests {
     @GET("/site/ci/api/training/getSpecialties/{id}") Call<Specialty.Array> getSpecialties(@Path("id") int ID);
 
     @GET("/site/ci/api/base/getEvents/{smeshenie}") Call<Event.Array> getEvents(@Path("smeshenie") int smeshenie);
+
+    @GET("/site/ci/api/base/getNews/{smeshenie}") Call<News.Array> getNews(@Path("smeshenie") int smeshenie);
 }
