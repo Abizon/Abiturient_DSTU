b1713d0bb91af8aff05184fca903d9a2cc25f606
dieselcoding <dieselcoding@gmail.com>
2017-11-25 16:35:39 +0300

    Diesel Commit

---
+++ app/src/main/java/com/dstu/myapplication/dstu/Requests.java
@@ -0,0 +1,14 @@
+package com.dstu.myapplication.dstu;
+
+import com.dstu.myapplication.models.Specialties;
+import com.dstu.myapplication.models.Facultie;
+
+import retrofit.Call;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+public interface Requests {
+    @GET("/site/ci/api/training/getChildren") Call<Facultie.Answer> getFaculties();
+
+    @GET("/site/ci/api/training/getSpecialties/{id}") Call<Specialties.Array> getSpecialties(@Path("id") int ID);
+}
