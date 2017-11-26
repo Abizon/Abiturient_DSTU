5df1e5a4610b4b10b1acfdefcfdd6dab1c0c08ad
Анна <hachaturovsergei@gmail.com>
2017-11-26 13:51:11 +0300

    Anya commit

--- app/src/main/java/com/dstu/myapplication/activities/SplashActivity.java
+++ app/src/main/java/com/dstu/myapplication/activities/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.dstu.myapplication.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +15,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#FF5722"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
+
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
